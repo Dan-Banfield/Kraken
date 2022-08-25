f640e9eb5b447e31147ed6b6fa489a2a90685bb5
Dan Banfield <dandbanfield@gmail.com>
2022-08-25 15:22:48 +0100

    Add project files.

---
+++ Kraken/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Kraken
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

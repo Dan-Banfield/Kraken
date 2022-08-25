dff1b2b0aab7b34251d6f055ed4ee94d94c58965
Dan Banfield <dandbanfield@gmail.com>
2022-08-25 16:48:08 +0100

    Update code-behind.

--- Kraken/View/MainWindow.axaml.cs
+++ Kraken/View/MainWindow.axaml.cs
@@ -4,9 +4,48 @@ namespace Kraken
 {
     public partial class MainWindow : Window
     {
+        #region Private Variables
+
+        private string selectedDirectory = string.Empty;
+
+        #endregion
+
+        #region Global Variables
+
+        public enum STATUS
+        {
+            READY,
+            BUSY
+        }
+
+        public STATUS CURRENT_STATUS = STATUS.READY;
+
+        public string SelectedDirectory
+        {
+            get
+            {
+                return selectedDirectory;
+            }
+            set
+            {
+                selectedDirectory = value;
+                selectedDirectoryTextBox.Text = value;
+            }
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #region Methods
+
+        #endregion
     }
 }
